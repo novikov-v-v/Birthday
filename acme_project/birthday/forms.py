1a28d75b17d184728f06949b0460b5269956585a
Vladimir Novikov <novikov-vl@yandex.ru>
2025-08-22 16:24:00 +0300

    Исправление last_name и приветствие. Урок 9-2-3.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class BirthdayForm(forms.Form):
     first_name = forms.CharField(label='Имя', max_length=20)
-    second_name = forms.CharField(
+    last_name = forms.CharField(
         label='Фамилия', required=False, help_text='Необязательное поле'
     )
     birthday = forms.DateField(
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,6 +4,7 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
+    print(request.GET)
     form = BirthdayForm()
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context)
