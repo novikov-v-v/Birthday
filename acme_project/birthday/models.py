66b0adca9a592192c4677eb17165136fa6ad7e4a
Vladimir Novikov <novikov-vl@yandex.ru>
2025-08-24 17:29:01 +0300

    Добавлены валидаторы и модель в админ-зоне. Урок 9-2-9.

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        verbose_name = 'день рождения'
+        verbose_name_plural = 'Дни рождения'
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
