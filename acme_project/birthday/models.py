060b84b52e4bc56277c5027698dc7e84d04de782
Vladimir Novikov <novikov-vl@yandex.ru>
2025-08-30 23:18:35 +0300

    Навели порядок. Урок 9-3-3.

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from .validators import real_age
 
@@ -20,6 +21,9 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+    def get_absolute_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -8,14 +8,25 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayMixin:
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 4
+
+
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
 class BirthdayDetailView(DetailView):
@@ -27,21 +38,3 @@ class BirthdayDetailView(DetailView):
             self.object.birthday
         )
         return context
-
-
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
-
-
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    pass
-
-
-class BirthdayListView(ListView):
-    model = Birthday
-    ordering = 'id'
-    paginate_by = 4
