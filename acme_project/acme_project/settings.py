14381a2f244e46e612afaf18b7b5794d379a18f8
Vladimir Novikov <novikov-vl@yandex.ru>
2025-09-14 15:11:47 +0300

    Добавлены кастомные страницы ошибок. Урок 10-2-1.

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -6,7 +6,10 @@ SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -16,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
+    'core.apps.CoreConfig',
     'pages.apps.PagesConfig',
     'django_bootstrap5',
 ]
@@ -35,6 +39,8 @@ ROOT_URLCONF = 'acme_project.urls'
 TEMPLATES_DIR = BASE_DIR / 'templates'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 # Подключаем бэкенд filebased.EmailBackend:
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'core.views.page_not_found'
